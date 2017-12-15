4536d38f055ad2ebbc53e7914fe0514fb82e57d1
INX <linw7@mail2.sysu.edu.cn>
2017-12-16 02:45:50 +0800

    add

--- app/src/main/java/com/example/lele/protoui/GatewayActivity.java
+++ app/src/main/java/com/example/lele/protoui/GatewayActivity.java
@@ -17,113 +17,56 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 
 public class GatewayActivity extends AppCompatActivity {
-
     private PieChart mChart;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gateway);
-
         mChart = (PieChart) findViewById(R.id.spread_pie_chart);
-        PieData mPieData = getPieData(4, 100);
+
+        PieData mPieData = getPieData();
         showChart(mChart, mPieData);
     }
 
     private void showChart(PieChart pieChart, PieData pieData) {
-        // pieChart.setHoleColorTransparent(true);
-
         pieChart.setHoleRadius(60f);  //半径
         pieChart.setTransparentCircleRadius(64f); // 半透明圈
-        //pieChart.setHoleRadius(0)  //实心圆
-
-        pieChart.setDescription("测试饼状图");
-
-        // mChart.setDrawYValues(true);
+        pieChart.setDescription("Activity");
         pieChart.setDrawCenterText(true);  //饼状图中间可以添加文字
-
         pieChart.setDrawHoleEnabled(true);
-
         pieChart.setRotationAngle(90); // 初始旋转角度
-
-        // draws the corresponding description value into the slice
-        // mChart.setDrawXValues(true);
-
-        // enable rotation of the chart by touch
         pieChart.setRotationEnabled(true); // 可以手动旋转
-
-        // display percentage values
         pieChart.setUsePercentValues(true);  //显示成百分比
-        // mChart.setUnit(" €");
-        // mChart.setDrawUnitsInChart(true);
-
-        // add a selection listener
-//      mChart.setOnChartValueSelectedListener(this);
-        // mChart.setTouchEnabled(false);
-
-//      mChart.setOnAnimationListener(this);
-
-        pieChart.setCenterText("Quarterly Revenue");  //饼状图中间的文字
-
-        //设置数据
-        pieChart.setData(pieData);
-
-        // undo all highlights
-//      pieChart.highlightValues(null);
-//      pieChart.invalidate();
-
+        pieChart.setCenterText("HAR");  //饼状图中间的文字
+        pieChart.setData(pieData); //设置数据
         Legend mLegend = pieChart.getLegend();  //设置比例图
         mLegend.setPosition(LegendPosition.RIGHT_OF_CHART);  //最右边显示
-//      mLegend.setForm(LegendForm.LINE);  //设置比例图的形状，默认是方形
         mLegend.setXEntrySpace(7f);
         mLegend.setYEntrySpace(5f);
-
         pieChart.animateXY(1000, 1000);  //设置动画
-        // mChart.spin(2000, 0, 360);
     }
 
-    /**
-     *
-     * @param count 分成几部分
-     * @param range
-     */
-    private PieData getPieData(int count, float range) {
-
+    private PieData getPieData() {
         ArrayList<String> xValues = new ArrayList<String>();  //xVals用来表示每个饼块上的内容
-
-        for (int i = 0; i < count; i++) {
-            xValues.add("Quarterly" + (i + 1));  //饼块上显示成Quarterly1, Quarterly2, Quarterly3, Quarterly4
-        }
+        xValues.add("Jog");  //饼块上显示成Quarterly1, Quarterly2, Quarterly3, Quarterly4
+        xValues.add("Walk");
+        xValues.add("Sit");
+        xValues.add("Stand");
 
         ArrayList<Entry> yValues = new ArrayList<Entry>();  //yVals用来表示封装每个饼块的实际数据
+        yValues.add(new Entry(14, 0));
+        yValues.add(new Entry(14, 1));
+        yValues.add(new Entry(34, 2));
+        yValues.add(new Entry(38, 3));
 
-        // 饼图数据
-        /**
-         * 将一个饼形图分成四部分， 四部分的数值比例为14:14:34:38
-         * 所以 14代表的百分比就是14%
-         */
-        float quarterly1 = 14;
-        float quarterly2 = 14;
-        float quarterly3 = 34;
-        float quarterly4 = 38;
-
-        yValues.add(new Entry(quarterly1, 0));
-        yValues.add(new Entry(quarterly2, 1));
-        yValues.add(new Entry(quarterly3, 2));
-        yValues.add(new Entry(quarterly4, 3));
-
-        //y轴的集合
-        PieDataSet pieDataSet = new PieDataSet(yValues, "Quarterly Revenue 2014"/*显示在比例图上*/);
+        PieDataSet pieDataSet = new PieDataSet(yValues, "Activity"/*显示在比例图上*/);
         pieDataSet.setSliceSpace(0f); //设置个饼状图之间的距离
 
         ArrayList<Integer> colors = new ArrayList<Integer>();
-
-        // 饼图颜色
         colors.add(Color.rgb(205, 205, 205));
         colors.add(Color.rgb(114, 188, 223));
         colors.add(Color.rgb(255, 123, 124));
         colors.add(Color.rgb(57, 135, 200));
-
         pieDataSet.setColors(colors);
 
         DisplayMetrics metrics = getResources().getDisplayMetrics();
@@ -131,8 +74,6 @@ public class GatewayActivity extends AppCompatActivity {
         pieDataSet.setSelectionShift(px); // 选中态多出的长度
 
         PieData pieData = new PieData(xValues, pieDataSet);
-
         return pieData;
     }
-
 }
