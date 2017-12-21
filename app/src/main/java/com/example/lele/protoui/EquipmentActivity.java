30a68ff83b0f71c6021fc89253e1c57f387f5404
INX <linw7@mail2.sysu.edu.cn>
2017-12-22 00:48:30 +0800

    fix Tab Bug

--- app/src/main/java/com/example/lele/protoui/EquipmentActivity.java
+++ app/src/main/java/com/example/lele/protoui/EquipmentActivity.java
@@ -1,27 +1,42 @@
 package com.example.lele.protoui;
 
+import android.app.ActivityGroup;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
 
 
-public class EquipmentActivity extends AppCompatActivity {
+public class EquipmentActivity extends ActivityGroup {
 
     private TabHost tabhost;
+    private ImageView title_bar_back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_equipment);
 
+        title_bar_back_btn = (ImageView)findViewById(R.id.title_bar_back_btn);
+
         tabhost = (TabHost) findViewById(android.R.id.tabhost);
         tabhost.setup();    //初始化TabHost组件
+
+        tabhost.setup(this.getLocalActivityManager());
 
         //添加第一个标签页
         tabhost.addTab(tabhost.newTabSpec("tab").setIndicator("手机模式").setContent(new Intent(this, CollectActivity.class)));
 
         //添加第二个标签页
         tabhost.addTab(tabhost.newTabSpec("tab1").setIndicator("手环模式").setContent(new Intent(this, AdjustActivity.class)));
+
+        title_bar_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(EquipmentActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
