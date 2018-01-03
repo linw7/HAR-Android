4a24c6137452682a5ca54d75aa4ee164ec513653
INX <linw7@mail2.sysu.edu.cn>
2018-01-03 18:08:30 +0800

    add

--- app/src/main/java/com/example/lele/protoui/RemindPhoneActivity.java
+++ app/src/main/java/com/example/lele/protoui/RemindPhoneActivity.java
@@ -2,11 +2,15 @@ package com.example.lele.protoui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TimePicker;
 
 public class RemindPhoneActivity extends AppCompatActivity {
 
     private TimePicker timepicker;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +18,18 @@ public class RemindPhoneActivity extends AppCompatActivity {
         setContentView(R.layout.activity_remind_phone);
 
         timepicker = (TimePicker) findViewById(R.id.timePicker);
+        button = (Button) findViewById(R.id.buttone);
+
+        timepicker.setIs24HourView(true);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hour = timepicker.getCurrentHour();
+                int minute = timepicker.getCurrentMinute();
+                Log.i("hour", hour + "");
+            }
+        });
 
     }
 }
